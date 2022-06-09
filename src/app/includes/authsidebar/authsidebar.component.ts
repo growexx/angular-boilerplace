0f5e0e5818d991d40d1deef41da4657c32dc3f5c
Pruthvi Dhamecha <pruthvi.dhamecha@growexx.com>
2022-06-09 16:27:31 +0530

    SignIn Module

---
+++ src/app/includes/authsidebar/authsidebar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-authsidebar',
+  templateUrl: './authsidebar.component.html',
+  styleUrls: ['./authsidebar.component.scss']
+})
+export class AuthsidebarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
