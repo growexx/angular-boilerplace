ad67720fbebcf297e96673379a5c465e1e5878f7
nirvi-growexxer <nirvi.shah@growexx.com>
2022-07-25 11:49:19 +0530

    task: code coverage and testing done

--- src/app/task/task.service.spec.ts
+++ src/app/task/task.service.spec.ts
@@ -1,16 +1,32 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { environment } from 'src/environments/environment';
 
 import { TaskService } from './task.service';
 
 describe('TaskService', () => {
   let service: TaskService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+    imports:[HttpClientTestingModule]
+    });
     service = TestBed.inject(TaskService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it(('should test getAllTask'),()=>{
+    const expectRes = {data:{}};
+    service.getAllTodos().subscribe(
+      res => expect(res).toEqual(expectRes,'should return expectedRes')
+    );
+    const req = httpTestingController.expectOne(`${environment.apiUrl1}/todos`)
+
+  })
 });
--- src/app/task/view-task/view-task.component.spec.ts
+++ src/app/task/view-task/view-task.component.spec.ts
@@ -1,16 +1,44 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
+import { TaskService } from '../task.service';
 
 import { ViewTaskComponent } from './view-task.component';
 
 describe('ViewTaskComponent', () => {
   let component: ViewTaskComponent;
   let fixture: ComponentFixture<ViewTaskComponent>;
+  let taskService: any;
+
+  taskService = jasmine.createSpyObj('TaskService', ['getAllTodos']);
+  const task = {
+    "todos": [
+      {
+        "id": 1,
+        "todo": "Do something nice for someone I care about",
+        "completed": true,
+        "userId": 26
+      },
+
+    ],
+    "total": 150,
+    "skip": 0,
+    "limit": 30
+  }
+
+  taskService.getAllTodos.and.returnValue(of(task))
+
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ViewTaskComponent ]
+      declarations: [ViewTaskComponent],
+      imports: [HttpClientTestingModule, RouterTestingModule],
+      providers: [
+        { provide: TaskService, useValue: taskService }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -22,4 +50,9 @@ describe('ViewTaskComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call getAllTasks', () => {
+    component.getAllTasks()
+    expect(component).toBeTruthy();
+  })
 });
