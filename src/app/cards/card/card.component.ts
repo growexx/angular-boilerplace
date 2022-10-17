bea208c5b1bf5fd61b82707fd3404b3ce1f15819
nirvi-growexxer <nirvi.shah@growexx.com>
2022-10-17 10:33:30 +0530

    card with icon added

--- src/app/cards/card/card.component.ts
+++ src/app/cards/card/card.component.ts
@@ -9,12 +9,13 @@ import { cardType } from 'src/app/core/interface/card';
 export class CardComponent implements OnInit {
 
   cardData: cardType = {
-    type: ['simple','withIconText'],
+    type: ['simple','withIcon','withIconText'],
     title: 'Simple Card',
     content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and"+
     "scrambled.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has"+
     "been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of"+
     "type and scrambled.",
+    icon: "fa-solid fa-cube",
     iconText: 'settings'
   }
   constructor() { }
